ad5ec5c727ab4f286f56629f32f5dbb42751be0d
Jason Hand <jason.hand@datadoghq.com>
2023-10-19 14:37:56 -0600

    New Version

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ datadogRum.init({
     service:'generative-debrief',
     env:'azure-production',
     // Specify a version number to identify the deployed version of your application in Datadog 
-    version: '1.0.0', 
+    version: '1.0.1', 
     sessionSampleRate:100,
     sessionReplaySampleRate: 100,
     trackUserInteractions: true,
