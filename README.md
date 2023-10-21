09bf4842210078d5cbd51b15716c42987e6a596d
Jason Hand <jason.hand@datadoghq.com>
2023-10-21 08:17:10 -0600

    1.0.4 Release - added RUM to index.js

--- README.md
+++ README.md
@@ -6,4 +6,13 @@ This is a basic React app to display a form for users to submit details about a 
 
 From there, the Logic App processes the submission and sends prompts to ChatGPT to generate a new response which is then displayed to the user.
 
+## Resources
+(Links coming soon...)
+- OpenAI Integration
+- OpenAI Integration Blog
+- Azure Integration
+- Logic App Integration Blog
+- RUM Documentation
+- RUM Blog
+
 ![UI screenshot](images/screenshot.png)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//RUM Integration //
+import { datadogRum } from '@datadog/browser-rum';
+//
+
+datadogRum.init({
+    applicationId: '866db0f2-7371-4d4a-8b77-340c5e498544',
+    clientToken: 'pub5096d6768cfbb8aa98b73877207a7bec',
+    site: 'datadoghq.com',
+    service:'generative-debrief',
+    env:'azure-production',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    version: '1.0.4', 
+    sessionSampleRate:100,
+    sessionReplaySampleRate: 100,
+    trackUserInteractions: true,
+    trackFrustrations: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
+
+// End RUM Integration
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
